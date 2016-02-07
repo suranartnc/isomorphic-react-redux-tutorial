97ef8bb378b9b19ed09cb64a0fd61bd4ab29b55a
SuranartN <suranartnc@gmail.com>
2016-02-07 18:29:27 +0700

    remove unused components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 import Header from './Header';
-import Main from './Main';
-import Sidebar from './Sidebar';
 import Footer from './Footer';
 
 const App = (props) => {
