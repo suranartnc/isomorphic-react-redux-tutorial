270bb09a12d61026995356b1af21512342a928c6
SuranartN <suranartnc@gmail.com>
2016-02-07 15:32:41 +0700

    iterate an array using array.prototype.map()

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
 const Main = () => {
+
+	const articles = [
+		{
+			id: 1,
+			title: 'Title 1'
+		}, {
+			id: 2,
+			title: 'Title 2'
+		}, {
+			id: 3,
+			title: 'Title 3'
+		}
+	];
+
 	return (
-		<div>Main</div>
+		<div>
+			{articles.map(function(article, index) {
+				return (
+					<article>
+						<h2>{ article.title }</h2>
+					</article>
+				);
+			})}
+		</div>
 	);
 }
 
