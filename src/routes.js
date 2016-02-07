3398136a15d264fcfb6c33630e5daaae541c9409
SuranartN <suranartnc@gmail.com>
2016-02-07 20:45:34 +0700

    rename Main to Home

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
-import Main from './components/Main';
+import Home from './components/Home';
 import ArticleDetail from './components/ArticleDetail';
 
 export default (
 	<Route path={`/`} component={App}>
-		<IndexRoute component={Main} />
+		<IndexRoute component={Home} />
 		<Route path={`questions/:question_id`} component={ArticleDetail} />
 	</Route>
 );
