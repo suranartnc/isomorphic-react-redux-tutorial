e2140f90515fbdc4b151b859602a0b39ce675c1a
SuranartN <suranartnc@gmail.com>
2016-02-07 18:39:57 +0700

    remove # from url

--- src/client.js
+++ src/client.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Router, Route, IndexRoute } from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory'
+const history = createBrowserHistory();
 
 import App from './components/App';
 import Main from './components/Main';
 import ArticleDetail from './components/ArticleDetail';
 
 ReactDOM.render(
-	<Router>
+	<Router history={history}>
 		<Route path={`/`} component={App}>
 			<IndexRoute component={Main} />
 			<Route path={`questions/:question_id`} component={ArticleDetail} />
