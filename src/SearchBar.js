15a2bbb2befe279839a7c658040f128986dd8546
SuranartN <suranartnc@gmail.com>
2016-02-07 17:05:53 +0700

    promote SearchBar to class component

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchForm = () => {
-	return (
-		<form>
-			<input type="text" placeholder="Search" />
-			<button>Submit</button>
-		</form>
-	);
+export default class SearchForm extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<form>
+				<input type="text" placeholder="Search" />
+				<button>Submit</button>
+			</form>
+		);
+	}
 }
-
-export default SearchForm;
