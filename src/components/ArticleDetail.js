381fd6f897a6e945dc30f4034d25d8fd8fd7e90c
SuranartN <suranartnc@gmail.com>
2016-02-07 20:53:04 +0700

    fetch article data using the parameter given by react router

--- src/components/ArticleDetail.js
+++ src/components/ArticleDetail.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ArticleDetail = (props) => {
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
-	return (
-		<article>
-			<h1>{ props.params.question_id }</h1>
-		</article>
-	);
+const API_URL = 'https://api.stackexchange.com/2.2/';
+
+function fetchData(url, callback) {
+	fetch(url)
+	  	.then(response => {
+	    	if (response.status >= 400) {
+	      		throw new Error("Bad response from server");
+	    	}
+	    	return response.json();
+	  	})
+	  	.then(data => {
+	    	callback(data);
+	  	});
 }
 
-export default ArticleDetail;
+export default class ArticleDetail extends Component {
+	
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			article: {}
+		}
+
+		this.getArticleById(this.props.params.question_id);
+	}
+
+	getArticleById(id) {
+		fetchData(`${API_URL}questions/${id}?order=desc&sort=activity&site=stackoverflow&filter=withbody`, (data) => {
+			this.setState({
+				article: data.items[0]
+			});
+		});
+	}
+
+	render() {
+		return (
+			<article>
+				<h1>{ this.state.article.title }</h1>
+				<div dangerouslySetInnerHTML={{__html: this.state.article.body }} />
+			</article>
+		);
+	}
+}
