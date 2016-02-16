a25fb853e0c9ba62c440c5a56b0d9972faaa6c9d
TaToonz <peerapath.t@gmail.com>
2016-02-16 18:24:58 +0700

    added redux-thunk middlewares

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -1,10 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 
+import thunk from 'redux-thunk';
 import clientMiddleware from './middlewares/clientMiddleware';
 import loggerMiddleware from './middlewares/loggerMiddleware';
 
 import reducers from './reducers';
 
 export default (initialState) => {
-	return applyMiddleware(clientMiddleware, loggerMiddleware)(createStore)(reducers, initialState);
+	const middlewares = [
+		thunk,
+		clientMiddleware,
+		loggerMiddleware
+	];
+
+	return applyMiddleware(...middlewares)(createStore)(reducers, initialState);
 }
